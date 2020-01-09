15c19c56acb0f63f7b993f960b330b3f1f2fb9ed
zhaolong <zhaolong@bjca.org.cn>
2020-01-09 13:30:45 +0800

    add packages

---
+++ DB2TemplateGenerator/Models/ColumnInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace DB2TemplateGenerator.Models
+{
+    public class ColumnInfo
+    {
+        /// <summary>
+        /// 字段名称
+        /// </summary>
+        public string ColumnName { get; set; }
+        /// <summary>
+        /// 字段类型
+        /// </summary>
+        public string ColumnType { get; set; }
+        /// <summary>
+        /// 字段注释
+        /// </summary>
+        public string ColumnComment { get; set; }
+
+        public ColumnInfo(string columnName, string columnType, string columnComment)
+        {
+            ColumnName = columnName;
+            ColumnType = columnType;
+            ColumnComment = columnComment;
+        }
+    }
+}
---
+++ DB2TemplateGenerator/Models/TableInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DB2TemplateGenerator.Models
+{
+    public class TableInfo
+    {
+        /// <summary>
+        /// 表名称
+        /// </summary>
+        public string TableName { get; set; }
+        /// <summary>
+        /// 表注释
+        /// </summary>
+        public string TableComment { get; set; }
+        /// <summary>
+        /// 表内字段
+        /// </summary>
+        public List<ColumnInfo> TableColumns { get; set; }
+
+        public TableInfo(string tableName, string tableComment)
+        {
+            TableName = tableName;
+            TableComment = tableComment;
+            TableColumns = new List<ColumnInfo>();
+        }
+    }
+}
